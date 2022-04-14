cbd28340ee244852965d6b42fb248f472ad796ee
maxsj462 <maxsj462@student.liu.se>
2022-04-14 14:58:37 +0200

    Solved merge conflicts

--- app/src/main/java/com/example/sub/MainActivity.kt
+++ app/src/main/java/com/example/sub/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.sub
 
+import com.example.sub.SignalingClient
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
@@ -11,6 +12,15 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.sub.databinding.ActivityPermissionBinding
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+import kotlinx.serialization.Serializable
+import org.json.JSONObject
+import kotlinx.coroutines.launch
+
+import com.example.sub.session.SignalWebsocketListener
+import okhttp3.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     private lateinit var layout: View
@@ -64,8 +74,30 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    @Serializable
+    data class User(val Reason : String, val Email : String, val PhoneNumber : String, val Name: String, val Password: String)
+
+    fun onClickSignalServer(view: View){
+        // wss test
+        val client = OkHttpClient.Builder()
+            .readTimeout(3, TimeUnit.SECONDS)
+            //.sslSocketFactory()
+            .build()
+        val request = Request.Builder()
+            .url("ws://144.24.171.133:4000") // 'ws'
+            .build()
+        val wsListener = SignalWebsocketListener ()
+        val webSocket = client.newWebSocket(request, wsListener) // this provide to make 'Open ws connection'
 
 
+        /*
+        val data = User("connect", "test@domain.com", "222333", "Edward Blom", "password123")
+        val dataString = Json.encodeToString(data)
+        println(dataString)
+        val sc = SignalingClient()
+        sc.sendCommand(dataString)
+        */
+    }
 }
 
 fun View.showSnackbar(
@@ -84,6 +116,3 @@ fun View.showSnackbar(
         snackbar.show()
     }
 }
-
-
-
