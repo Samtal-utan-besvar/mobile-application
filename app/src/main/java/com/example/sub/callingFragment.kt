0b060c74b62e4eac0b12ea9bbe2be6e6e3ddfa0e
Josef Karlsson <joska986@student.liu.se>
2022-04-18 11:53:09 +0200

    17 - feat: Added AudioManager so that the speakers can be turned on or off

--- app/src/main/java/com/example/sub/callingFragment.kt
+++ app/src/main/java/com/example/sub/callingFragment.kt
@@ -1,13 +1,17 @@
 package com.example.sub
 
-import android.R.attr.data
+import android.content.Context
+import android.media.AudioManager
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Chronometer
 import android.widget.Toast
 import android.widget.ToggleButton
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation.findNavController
@@ -25,19 +29,30 @@ private const val ARG_PARAM2 = "param2"
  */
 class callingFragment : Fragment() {
     private var navController: NavController? = null
+
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(R.layout.fragment_calling, container, false)
+
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         navController = findNavController(view.findViewById(R.id.closeCall))
+
+        val audioManager = activity?.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_UNMUTE, 0)   // turn speakers on if there aren't already
+
+        // audio sample, remove when webRTC is implemented
+        val mediaPlayer = MediaPlayer.create(context, R.raw.sample_sound)
+        mediaPlayer.start()
+
         view.findViewById<View>(R.id.closeCall).setOnClickListener {
-
             // TODO: Action when close call, disconnect call from server??
-
             navController!!.navigate(
                 R.id.action_callingFragment_to_userProfile
             )
@@ -48,9 +63,11 @@ class callingFragment : Fragment() {
         toggleButtonSilentMode.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
                 // TODO: Action when speaker is on
+                audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_UNMUTE, 0);
                 Toast.makeText(activity, "speaker on", Toast.LENGTH_LONG).show()    // remove
             } else {
                 // TODO: Action when speaker is off
+                audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_MUTE, 0);
                 Toast.makeText(activity, "speaker off", Toast.LENGTH_LONG).show()    // remove
             }
         }
@@ -60,9 +77,11 @@ class callingFragment : Fragment() {
         toggleButtonMute.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
                 // TODO: Action when un-muted
+                audioManager.isMicrophoneMute = false   // not tested
                 Toast.makeText(activity, "un-mute", Toast.LENGTH_LONG).show()       // remove
             } else {
                 // TODO: Action when muted
+                audioManager.isMicrophoneMute = true   // not tested
                 Toast.makeText(activity, "mute", Toast.LENGTH_LONG).show()          // remove
             }
         }
