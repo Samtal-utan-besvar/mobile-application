0241edea437846464721cd1d8820912c1ba7a9a3
maxsj462 <maxsj462@student.liu.se>
2022-04-26 15:43:45 +0200

    3B - Kept on going with design for contactlist, now the drop down menu works.. not possible to log out though

--- app/src/main/java/com/example/sub/ProfileFragment.kt
+++ app/src/main/java/com/example/sub/ProfileFragment.kt
@@ -2,20 +2,21 @@ package com.example.sub
 
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Spinner
-import android.widget.TextView
+import android.view.*
+import android.widget.*
+
+import com.google.android.material.textfield.TextInputLayout
+
 import androidx.constraintlayout.widget.Group
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sub.ui.login.LoginViewModel
@@ -25,7 +26,7 @@ import kotlinx.coroutines.runBlocking
 /** The ProfileFragment which shows the "Homescreen" and the Contactlist of the
  * specified user
  */
-class ProfileFragment : Fragment(), contactListAdapter.ListItemClickListener {
+class ProfileFragment : Fragment(), contactListAdapter.ListItemClickListener, PopupMenu.OnMenuItemClickListener {
     var navController: NavController? = null
 
     private lateinit var loginViewModel: LoginViewModel
@@ -44,7 +45,7 @@ class ProfileFragment : Fragment(), contactListAdapter.ListItemClickListener {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
@@ -107,15 +108,53 @@ class ProfileFragment : Fragment(), contactListAdapter.ListItemClickListener {
         }
         Log.d("myDebug", "getUserToken(): " + getUserToken())
 
-
-        val spinner: Spinner = view.findViewById<View>(R.id.spinner) as Spinner
-        ArrayAdapter.createFromResource(this, R.array.logout_array, android.R.layout.simple_spinner_item).also { adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            spinner.adapter = adapter
+        view.findViewById<View>(R.id.settingsButton).setOnClickListener{
+            showMenu(view.findViewById(R.id.settingsButton))
+        }
+        /**val spinner: Spinner = view.findViewById<View>(R.id.spinner) as Spinner
+        this.context?.let {
+            ArrayAdapter.createFromResource(it, R.array.logout_array, android.R.layout.simple_spinner_item).also { adapter ->
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                spinner.adapter = adapter
+            }
+        }**/
+    }
+    private fun showMenu(v: View) {
+        PopupMenu(this.context, v).apply {
+            setOnMenuItemClickListener(this@ProfileFragment)
+            inflate(R.menu.logout_menu)
+            show()
         }
     }
 
+    override fun onMenuItemClick(item: MenuItem): Boolean {
+        println(R.id.loggaut)
+        println("itemid: ")
+        println(item.itemId)
+        return when (item.itemId) {
+            R.id.loggaut -> {
+                loginViewModel.loginRepository.logout()
+                true
+            }
+            else -> false
+        }
+    }
 
+    /**
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        val inflater = MenuInflater(this.context)
+        inflater.inflate(R.menu.logout_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.ProfileFragment){
+            println("HOPPLA POLLE")
+            return true
+        }
+        return NavigationUI.onNavDestinationSelected(item!!,
+            requireView().findNavController())
+                || super.onOptionsItemSelected(item)
+    }**/
 
     /** Fetches the username from the LoginViewModel class **/
     private fun getUserName(): String? {
